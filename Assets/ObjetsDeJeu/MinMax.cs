85628e5fff59a729d51835b07743315589308ad2
Elow <kevin.basin@outlook.com>
2014-02-25 09:00:34 +0100

    Modif IA et préparation MinMax

---
+++ Assets/ObjetsDeJeu/MinMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.ObjetsDeJeu
+{
+    class MinMax : Player, IAPlayer
+    {
+        public MinMax(string name, PlayerColor color) : base(name, color)
+        {
+            // @TODO
+        }
+
+        public Coordonnees GetBestMove(Goban goban)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        private int getMin(int profondeur)
+        {
+            return 0;
+        }
+
+        private int getMax(int profondeur)
+        {
+            return 0;
+        }
+
+        private int getSeries()
+        {
+            return 0;
+        }
+    }
+}
